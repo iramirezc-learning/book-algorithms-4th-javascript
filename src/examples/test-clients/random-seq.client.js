b319d4af1385ea30513a7ce50b10181f401a6ec9
Isaac Ramirez <iramirezc@live.com.mx>
2020-04-18 21:32:17 -0500

    add RandomSeq client

---
+++ src/examples/test-clients/random-seq.client.js
@@ -0,0 +1,38 @@
+const { StdRandom, StdOut } = require('../../libs')
+
+/**
+ * RandomSeq
+ * @desc Sample StdOut client
+ * @see p. 37
+ */
+class RandomSeq {
+  /**
+   * main
+   * @desc Prints `n` random real numbers in range [lo, hi).
+   * @param {[]} args [n, lo, hi]
+   * @example
+   * ```sh
+   * $ node random-seq.client.js 5 100.0 199.99
+   * 166.29
+   * 171.09
+   * 199.85
+   * 159.78
+   * 165.14
+   * ```
+   */
+  static main (args) {
+    const n = parseInt(args[0], 10)
+    const lo = parseFloat(args[1])
+    const hi = parseFloat(args[2])
+
+    for (let i = 0; i < n; i++) {
+      const x = StdRandom.uniform(lo, hi)
+
+      StdOut.printf('%f', x.toFixed(2))
+    }
+  }
+}
+
+// Execution
+// ==============================
+RandomSeq.main(process.argv.slice(2))
