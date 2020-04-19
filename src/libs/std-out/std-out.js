465a8fca13ee9767f6c30f99b99f68c727458f8b
Isaac Ramirez <iramirezc@live.com.mx>
2020-04-18 22:18:44 -0500

    update jsdocs

--- src/libs/std-out/std-out.js
+++ src/libs/std-out/std-out.js
@@ -3,6 +3,7 @@ const format = require('util').format
 class StdOut {
   /**
    * Prints formatted data in the `stdout`.
+   * @see {@link https://nodejs.org/docs/latest-v12.x/api/util.html#util_util_format_format_args}
    * @param {*} any
    */
   static printf () {
@@ -11,6 +12,8 @@ class StdOut {
 
   /**
    * Prints formatted data with a new line in the `stdout`.
+   * @see {@link https://nodejs.org/docs/latest-v12.x/api/util.html#util_util_format_format_args}
+   * @param {*} any
    */
   static println () {
     process.stdout.write(format(...arguments, '\n'))
