981cdd62344671bc0420aa4e90ddf79cd50943b3
Isaac Ramirez <iramirezc@live.com.mx>
2020-04-18 22:09:24 -0500

    replace printf with println

--- src/examples/test-clients/std-random.client.js
+++ src/examples/test-clients/std-random.client.js
@@ -22,30 +22,30 @@ class StdRandomClient {
    * ```
    */
   static main () {
-    StdOut.printf('uniform method')
-    StdOut.printf('  real between [0, 1):', StdRandom.uniform())
-    StdOut.printf('  integer between [0, 1):', StdRandom.uniform(1))
-    StdOut.printf('  integer between [0, 5):', StdRandom.uniform(5))
-    StdOut.printf('  integer between [5, 10):', StdRandom.uniform(5, 10))
-    StdOut.printf('  real between [5.5, 9.9):', StdRandom.uniform(5.5, 9.9))
+    StdOut.println('uniform method')
+    StdOut.println('  real between [0, 1):', StdRandom.uniform())
+    StdOut.println('  integer between [0, 1):', StdRandom.uniform(1))
+    StdOut.println('  integer between [0, 5):', StdRandom.uniform(5))
+    StdOut.println('  integer between [5, 10):', StdRandom.uniform(5, 10))
+    StdOut.println('  real between [5.5, 9.9):', StdRandom.uniform(5.5, 9.9))
 
-    StdOut.printf('bernoulli method')
-    StdOut.printf('  probability of 0.5:', StdRandom.bernoulli())
-    StdOut.printf('  probability of 0.9:', StdRandom.bernoulli(0.9))
-    StdOut.printf('  probability of 0.1:', StdRandom.bernoulli(0.1))
+    StdOut.println('bernoulli method')
+    StdOut.println('  probability of 0.5:', StdRandom.bernoulli())
+    StdOut.println('  probability of 0.9:', StdRandom.bernoulli(0.9))
+    StdOut.println('  probability of 0.1:', StdRandom.bernoulli(0.1))
 
-    StdOut.printf('discrete method')
-    StdOut.printf('  discrete distribution:', StdRandom.discrete([0.1, 0.3, 0.0, 0.4, 0.1, 0.05, 0.05]))
+    StdOut.println('discrete method')
+    StdOut.println('  discrete distribution:', StdRandom.discrete([0.1, 0.3, 0.0, 0.4, 0.1, 0.05, 0.05]))
 
-    StdOut.printf('shuffle method')
+    StdOut.println('shuffle method')
 
     const integers = [1, 2, 3, 4, 5]
     StdRandom.shuffle(integers)
-    StdOut.printf('  shuffle array of integers [1, 2, 3, 4, 5] ->', integers)
+    StdOut.println('  shuffle array of integers [1, 2, 3, 4, 5] ->', integers)
 
     const chars = ['a', 'b', 'c', 'd', 'e']
     StdRandom.shuffle(chars)
-    StdOut.printf("  shuffle array of chars ['a', 'b', 'c', 'd', 'e'] ->", chars)
+    StdOut.println("  shuffle array of chars ['a', 'b', 'c', 'd', 'e'] ->", chars)
   }
 }
 
