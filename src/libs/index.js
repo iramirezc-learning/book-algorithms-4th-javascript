361d31ae77da86c73cc7be953c416c7a7cc943f4
Isaac Ramirez <iramirezc@live.com.mx>
2020-04-18 21:30:19 -0500

    implement StdOut lib

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,3 +1,4 @@
 module.exports = {
+  StdOut: require('./std-out/std-out'),
   StdRandom: require('./std-random/std-random')
 }
---
+++ src/libs/std-out/std-out.js
@@ -0,0 +1,11 @@
+class StdOut {
+  /**
+   * Prints to the console StdOut
+   * @param {*} any
+   */
+  static printf () {
+    console.log(...arguments)
+  }
+}
+
+module.exports = StdOut
