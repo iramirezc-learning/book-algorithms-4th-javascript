bbedcc070f4811ff3b89dc7a80ec290d2adc077e
Isaac Ramirez <iramirezc@live.com.mx>
2020-04-29 03:31:08 -0500

    implement pad util

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,6 +14,17 @@ function buildArrayOf (n, data) {
   return a
 }
 
+function pad (str, n = 0) {
+  const isLeftPad = n > 0
+  let remaining = Math.abs(n) - String(str).length
+  let pads = ''
+
+  while (remaining-- > 0) pads += ' '
+
+  return isLeftPad ? `${pads}${str}` : `${str}${pads}`
+}
+
 module.exports = {
-  buildArrayOf
+  buildArrayOf,
+  pad
 }
