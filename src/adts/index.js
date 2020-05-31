dab5797c1af4bc24e84a0152bf8244141da37654
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-31 03:04:08 -0500

    implement FixedStack ADT

--- src/adts/index.js
+++ src/adts/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   Accumulator: require('./accumulator/accumulator'),
   BasicDate: require('./basic-date/basic-date'),
+  FixedStack: require('./fixed-stack/fixed-stack'),
   Counter: require('./counter/counter'),
   Node: require('./node/node'),
   SmallDate: require('./small-date/small-date'),
