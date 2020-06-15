05d15a53720cab4055538f669e7aeef3e8b23f26
Isaac Ramirez <iramirezc@live.com.mx>
2020-06-14 22:49:29 -0500

    update dijstras two stack algorithm  with: - FixedCapactityStack ADT - ResizableArrayStack ADT

--- src/algorithms/dijkstra-two-stack/dijkstra-two-stack.spec.js
+++ src/algorithms/dijkstra-two-stack/dijkstra-two-stack.spec.js
@@ -1,5 +1,5 @@
 const DijkstraTwoStack = require('./dijkstra-two-stack')
-const { Stack } = require('../../adts')
+const { Stack, FixedCapacityStack, ResizableArrayStack } = require('../../adts')
 
 describe(`Unit Tests: Dijkstra's Two-Stack Algorithm for Expression Evaluation`, () => {
   describe('when using default Stack implementation', () => {
@@ -46,6 +46,44 @@ describe(`Unit Tests: Dijkstra's Two-Stack Algorithm for Expression Evaluation`,
     it('should evaluate an square root function', () => {
       const expression = '( ( 1 + sqrt ( 5.0 ) ) / 2.0 )'
       const expectedResult = 1.618033988749895
+
+      const result = DijkstraTwoStack.evaluate(expression)
+
+      expect(result).toBe(expectedResult)
+    })
+  })
+
+  describe('when using FixedCapacityStack implementation', () => {
+    beforeAll(() => {
+      DijkstraTwoStack.Stack = FixedCapacityStack
+    })
+
+    it('should be using FixedCapacityStack ADT implementation', () => {
+      expect(DijkstraTwoStack.Stack).toBe(FixedCapacityStack)
+    })
+
+    it('should evaluate an expression correctly', () => {
+      const expression = '( ( ( ( 1 + sqrt ( 5.0 ) ) / 2.0 ) * 2 ) - 1 )'
+      const expectedResult = 2.23606797749979
+
+      const result = DijkstraTwoStack.evaluate(expression)
+
+      expect(result).toBe(expectedResult)
+    })
+  })
+
+  describe('when using ResizableArrayStack implementation', () => {
+    beforeAll(() => {
+      DijkstraTwoStack.Stack = ResizableArrayStack
+    })
+
+    it('should be using ResizableArrayStack ADT implementation', () => {
+      expect(DijkstraTwoStack.Stack).toBe(ResizableArrayStack)
+    })
+
+    it('should evaluate an expression correctly', () => {
+      const expression = '( ( ( ( 1 + sqrt ( 5.0 ) ) / 2.0 ) * 2 ) - 1 )'
+      const expectedResult = 2.23606797749979
 
       const result = DijkstraTwoStack.evaluate(expression)
 
