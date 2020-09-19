cf2be6342dafcc193f6d196a97c066f200c57a6c
Isaac Ramirez <iramirezc@live.com.mx>
2020-09-19 11:41:02 -0500

    - implement WeightedQuickUnionUF algorithm

---
+++ src/algorithms/union-find-weighted/union-find-weighted.spec.js
@@ -0,0 +1,133 @@
+const WeightedQuickUnionUF = require('./union-find-weighted')
+
+describe('Unit Tests: Union Find Weighted Algorithm', () => {
+  beforeEach(() => {
+    this.sites = 10
+    this.pairs = [
+      [4, 3],
+      [3, 8],
+      [6, 5],
+      [9, 4],
+      [2, 1],
+      [8, 9],
+      [5, 0],
+      [7, 2],
+      [6, 1],
+      [1, 0],
+      [6, 7]
+    ]
+    this.wuf = new WeightedQuickUnionUF(this.sites)
+  })
+
+  describe('when initialized', () => {
+    it('should have a prop `_id` which is an array initialized with different components', () => {
+      const expectedIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      expect(this.wuf._id).toEqual(expectedIds)
+    })
+
+    it('should have a prop `_size` which is an array initialized to 1', () => {
+      const expectedSizes = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+      expect(this.wuf._size).toEqual(expectedSizes)
+    })
+
+    it('should have a property `_count` equal to the number of sites', () => {
+      expect(this.wuf._count).toBe(this.sites)
+    })
+
+    it('should not be extensible', () => {
+      const expectedProps = ['_id', '_size', '_count']
+
+      this.wuf.newProp = 'hello'
+
+      const actualProps = Object.getOwnPropertyNames(this.wuf)
+      expect(actualProps).toEqual(expectedProps)
+      expect(this.wuf.newProp).toBeUndefined()
+    })
+  })
+
+  describe('count method', () => {
+    it('should return the count to be the initial components count', () => {
+      expect(this.wuf.count()).toBe(this.sites)
+    })
+  })
+
+  describe('find method', () => {
+    it('should return the same component id as the site index when initialized', () => {
+      const sites = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      sites.forEach((site, index) => {
+        expect(this.wuf.find(site)).toBe(index)
+      })
+    })
+
+    it('should return the new component id if changed for a site', () => {
+      const siteIndex = 0
+      const newComponentId = 5
+      this.wuf._id[siteIndex] = newComponentId // change the componentId for site 0
+
+      expect(this.wuf.find(siteIndex)).toBe(newComponentId)
+    })
+  })
+
+  describe('connected method', () => {
+    it('should return true if `p` and `q` have the same component id', () => {
+      const p = 0 // first site
+      const q = 9 // last site
+      this.wuf._id[0] = 9 // force site with index 0 to have the same component id as site with index 9
+
+      expect(this.wuf.connected(p, q)).toBeTrue()
+    })
+
+    it('should return false if `p` and `q` have different components id', () => {
+      const p = 0 // first site with component id = 0
+      const q = 9 // last site with component id = 9
+
+      expect(this.wuf.connected(p, q)).toBeFalse()
+    })
+  })
+
+  describe('union method', () => {
+    it('should connect two sites by assigning them the same component id', () => {
+      const p = 0 // first site
+      const q = 9 // last site
+      const expectedIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+      this.wuf.union(p, q)
+
+      expect(this.wuf._id).toEqual(expectedIds)
+    })
+
+    it('should decrement the count of components by 1', () => {
+      const expectedCount = this.sites - 1
+
+      this.wuf.union(3, 4)
+
+      expect(this.wuf.count()).toBe(expectedCount)
+    })
+
+    it('should do nothing when sites are already connected', () => {
+      const p = 2 // 3rd site
+      const q = 5 // 6th site
+      const expectedIds = [0, 1, 2, 3, 4, 2, 6, 7, 8, 9]
+      const expectedCount = this.sites - 1
+
+      this.wuf.union(p, q)
+      this.wuf.union(q, p)
+
+      expect(this.wuf._id).toEqual(expectedIds)
+      expect(this.wuf.count()).toBe(expectedCount)
+    })
+
+    it('should connect the pairs given returning a total of 2 components', () => {
+      const expectedCount = 2
+
+      this.pairs.forEach(([p, q]) => {
+        this.wuf.union(p, q)
+      })
+
+      expect(this.wuf.count()).toBe(expectedCount)
+    })
+  })
+})
