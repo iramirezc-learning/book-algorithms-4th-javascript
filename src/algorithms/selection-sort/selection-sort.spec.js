f7266f5e55bb54df9237a11ebc0fc47b529c1a08
Isaac Ramirez <iramirezc@live.com.mx>
2020-09-23 01:28:28 -0500

    - refactor SelectionSort to extend GenericSort - refactor InsertionSort to extend GenericSort - removed duplicated sorting tests

--- src/algorithms/selection-sort/selection-sort.spec.js
+++ src/algorithms/selection-sort/selection-sort.spec.js
@@ -1,7 +1,6 @@
 const Selection = require('./selection-sort')
 const { newArrayOf } = require('../../utils')
 const { StdRandom } = require('../../libs')
-const { defaultComparator: comparator } = require('../../common')
 
 describe('Unit Tests: Selection Sort Algorithm', () => {
   beforeEach(() => {
@@ -11,97 +10,7 @@ describe('Unit Tests: Selection Sort Algorithm', () => {
     this.allEqualArray = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
   })
 
-  describe('static less method', () => {
-    it('should return true if a is less than b', () => {
-      const a = 1
-      const b = 2
-
-      expect(Selection.less(a, b, comparator)).toBe(true)
-    })
-
-    it('should return false if a is greater than b', () => {
-      const a = 2
-      const b = 1
-
-      expect(Selection.less(a, b, comparator)).toBe(false)
-    })
-
-    it('should return false if a is equal to b', () => {
-      const a = 1
-      const b = 1
-
-      expect(Selection.less(a, b, comparator)).toBe(false)
-    })
-
-    it('should have a defaultComparator function', () => {
-      expect(Selection.less(1, 10)).toBe(true)
-      expect(Selection.less(1, 1)).toBe(false)
-      expect(Selection.less(10, 1)).toBe(false)
-    })
-  })
-
-  describe('static exchange method', () => {
-    it('should interchange the values of the two given indexes in the array', () => {
-      const array = [5, 6, 7]
-      const expectedArray = [7, 6, 5]
-
-      Selection.exchange(array, 0, 2)
-
-      expect(array).toEqual(expectedArray)
-    })
-  })
-
-  describe('static isSorted method', () => {
-    it('should return true if an array is sorted', () => {
-      expect(Selection.isSorted(this.orderedArray)).toBe(true)
-      expect(Selection.isSorted(this.allEqualArray)).toBe(true)
-    })
-
-    it('should return false if the array is not sorted', () => {
-      expect(Selection.isSorted(this.unorderedArray)).toBe(false)
-      expect(Selection.isSorted(this.reversedArray)).toBe(false)
-    })
-
-    it('should accept a comparator function', () => {
-      const orderedArray = ['A', 'B', 'C']
-      const unorderedArray = ['Z', 'X', 'Y']
-
-      expect(Selection.isSorted(orderedArray, comparator)).toBe(true)
-      expect(Selection.isSorted(unorderedArray, comparator)).toBe(false)
-    })
-
-    it('should return true for a sorted big array', () => {
-      const n = 1000000 // a million!
-      const array = newArrayOf(n, i => i) // from 0 to 999999
-
-      expect(Selection.isSorted(array)).toBeTrue()
-    })
-
-    it('should return false for a random big array', () => {
-      const n = 1000000 // a million!
-      const array = newArrayOf(n, i => i) // from 0 to 999999
-      array[array.length - 1] = 0 // change last number to be 0
-
-      expect(Selection.isSorted(array)).toBeFalse()
-    })
-  })
-
-  describe('static show method', () => {
-    it('should be a function', () => {
-      // I could test the call to StdOut, but I don't want to.
-      expect(Selection.show).toBeInstanceOf(Function)
-    })
-  })
-
   describe('static sort method', () => {
-    it('should sort a unordered array', () => {
-      const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-      Selection.sort(this.unorderedArray)
-
-      expect(this.unorderedArray).toEqual(expectedArray)
-    })
-
     it('should sort an ordered array', () => {
       const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -116,6 +25,14 @@ describe('Unit Tests: Selection Sort Algorithm', () => {
       Selection.sort(this.reversedArray)
 
       expect(this.reversedArray).toEqual(expectedArray)
+    })
+
+    it('should sort a unordered array', () => {
+      const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+      Selection.sort(this.unorderedArray)
+
+      expect(this.unorderedArray).toEqual(expectedArray)
     })
 
     it('should sort an array with all the values to be equal', () => {
