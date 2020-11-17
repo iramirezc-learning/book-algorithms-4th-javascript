6301fc17db8e1d76a6c323f761917443fc573594
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-17 01:25:42 -0600

    - fix jsdoc - minor refactors

--- src/abstracts/generic-sort/generic-sort.js
+++ src/abstracts/generic-sort/generic-sort.js
@@ -2,50 +2,53 @@ const { StdOut } = require('../../libs')
 const { defaultComparator } = require('../../common')
 
 /**
- * GenericSort
- * @classdesc Abstract Class for Sorting Algorithms.
+ * @callback ComparatorFn
+ * @param {*} a - Comparable object `a`.
+ * @param {*} b - Comparable object `b`.
+ * @returns {number} Returns `-1` when `a` is less than `b`,
+ * returns `1` when `a` is greater than `b` or
+ * returns `0` when `a` is equal to `b`.
+ */
+
+/**
+ * Abstract Generic Class for Sorting Algorithms.
  */
 class GenericSort {
   /**
    * Returns whether `a` is less than `b`
-   * based on a comparing function.
-   * @param {*} a Comparable object `a`
-   * @param {*} b Comparable object `b`
-   * @param {function} comparator A comparing function that
-   * returns -1 when `a` is less than `b`,
-   * returns 1 when `a` is greater than `b` or
-   * returns 0 when `a` is equal to `b`.
+   * based on the comparing function.
+   * @param {*} a - Comparable object `a`.
+   * @param {*} b - Comparable object `b`.
+   * @param {ComparatorFn} [comparator=defaultComparator] - The comparator function.
+   * @returns {boolean} Returns true if `a` is less than `b`.
    */
   static less (a, b, comparator = this.defaultComparator) {
     return comparator(a, b) < 0
   }
 
   /**
-   * Interchanges the values located in indexes `i` and `j`
-   * for the given `array`.
-   * @param {[*]} array Array of values.
-   * @param {number} i Index 1
-   * @param {number} j Index 2
+   * Interchanges the values located at indexes `i` and `j`
+   * for the given array `a`.
+   * @param {Array<*>} a - The array of values.
+   * @param {number} i - The first index.
+   * @param {number} j - The second index.
    */
-  static exchange (array, i, j) {
-    const temp = array[i]
+  static exchange (a, i, j) {
+    const temp = a[i]
 
-    array[i] = array[j]
-    array[j] = temp
+    a[i] = a[j]
+    a[j] = temp
   }
 
   /**
-   * Returns `true` if the provided array is sorted,
-   * `false` otherwise.
-   * @param {[*]} array Array of values
-   * @param {function} comparator A comparing function that
-   * returns -1 when `a` is less than `b`,
-   * returns 1 when `a` is greater than `b` or
-   * returns 0 when `a` is equal to `b`.
+   * Returns `true` if the provided array is sorted.
+   * @param {Array<*>} a - The array of values.
+   * @param {ComparatorFn} [comparator=defaultComparator] - The comparator function.
+   * @returns {boolean} Whether the array is sorted or not.
    */
-  static isSorted (array, comparator = this.defaultComparator) {
-    for (let i = 1; i < array.length; i++) {
-      if (this.less(array[i], array[i - 1], comparator)) {
+  static isSorted (a, comparator = this.defaultComparator) {
+    for (let i = 1; i < a.length; i++) {
+      if (this.less(a[i], a[i - 1], comparator)) {
         return false
       }
     }
@@ -54,25 +57,34 @@ class GenericSort {
   }
 
   /**
-   * A function that prints to the StdOut
-   * the contents of the `array` provided.
-   * @param {[*]} array Array of values.
+   * Prints out the contents of the array `a`.
+   * @param {Array<*>} a - The array of values.
    */
-  static show (array) {
-    StdOut.println(array.join(' '))
+  static show (a) {
+    StdOut.println(a.join(' '))
   }
 
   /**
-   * Should sorts the `array` using the Sorting algorithm of your choice.
+   * Should sort the array `a` using the sorting algorithm of your choice.
    * @abstract
    * @throws SyntaxError This function should be implemented by the client.
+   * @param {Array<*>} a - The array of values.
    */
-  static sort () {
+  static sort (a) {
     throw new SyntaxError('sort method not implemented')
   }
 }
 
-// public final defaultComparator
+/**
+ * Default comparator function.
+ * @method defaultComparator
+ * @memberof GenericSort.
+ * @param {*} a - Comparable object `a`.
+ * @param {*} b - Comparable object `b`.
+ * @returns {number} Returns `-1` when `a` is less than `b`,
+ * returns `1` when `a` is greater than `b` or
+ * returns `0` when `a` is equal to `b`.
+ */
 Object.defineProperty(GenericSort, 'defaultComparator', { value: defaultComparator })
 
 module.exports = GenericSort
