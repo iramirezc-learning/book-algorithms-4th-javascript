408044b0c39e8834b6d57475406a64cd6ec3878d
Isaac Ramirez <iramirezc@live.com.mx>
2020-11-28 22:00:32 -0600

    - implement size, isEmpty & contains methods

--- src/abstracts/symbol-table/symbol-table.js
+++ src/abstracts/symbol-table/symbol-table.js
@@ -10,7 +10,18 @@ class SymbolTable {
    * @param {ComparatorFn} [comparator=defaultComparator] - The comparator function.
    */
   constructor (comparator = defaultComparator) {
+    /**
+     * Comparator function for the keys in the ST.
+     * @type {ComparatorFn}
+     */
     this.comparator = comparator
+
+    /**
+     * Size of the ST.
+     * @private
+     * @type {number}
+     */
+    this._n = 0
   }
 
   /**
@@ -47,34 +58,28 @@ class SymbolTable {
   }
 
   /**
-   * Returns `true` if the `key` has an associated value.
-   * @abstract
-   * @throws SyntaxError - This function should be implemented by the client.
-   * @param {*} key - The key to be inspected.
-   * @returns {boolean} Returns whether the key has a value associated or not.
+   * Returns the number of key-value pairs in the table.
+   * @returns {number} Returns the total key-value pairs in the table.
    */
-  contains (key) {
-    throw new SyntaxError('contains method is not implemented')
+  size () {
+    return this._n
   }
 
   /**
    * Returns `true` if the table has no keys, `false` otherwise.
-   * @abstract
-   * @throws SyntaxError - This function should be implemented by the client.
    * @returns {boolean} Returns whether the Symbol Table is empty or not.
    */
   isEmpty () {
-    throw new SyntaxError('isEmpty method is not implemented')
+    return this.size() === 0
   }
 
   /**
-   * Returns the number of key-value pairs in the table.
-   * @todo Implementation.
-   * @throws SyntaxError - This function should be implemented by the client.
-   * @returns {number} Returns the total key-value pairs in the table.
+   * Returns `true` if the `key` has an associated value.
+   * @param {*} key - The key to be inspected.
+   * @returns {boolean} Returns whether the key has a value associated or not.
    */
-  size () {
-    throw new SyntaxError('size method is not implemented')
+  contains (key) {
+    return this.get(key) !== null
   }
 
   /**
--- src/abstracts/symbol-table/symbol-table.spec.js
+++ src/abstracts/symbol-table/symbol-table.spec.js
@@ -9,12 +9,50 @@ describe('Unit Tests: Abstract SymbolTable', () => {
   describe('SymbolTable instance', () => {
     it('should implement the generic API', () => {
       const expectedAPI = [
-        'put', 'get', 'delete', 'contains', 'isEmpty', 'size', 'keys'
+        'put', 'get', 'delete', 'isEmpty', 'size', 'contains', 'keys'
       ]
 
       expectedAPI.forEach(method => {
         expect(this.st[method]).toBeInstanceOf(Function)
       })
+    })
+  })
+
+  describe('SymbolTable#size()', () => {
+    it('should return 0 when initialized', () => {
+      expect(this.st.size()).toBe(0)
+    })
+
+    it('should return the value of _n', () => {
+      this.st._n = 1
+
+      expect(this.st.size()).toBe(1)
+    })
+  })
+
+  describe('SymbolTable#isEmpty()', () => {
+    it('should return true when size is 0', () => {
+      expect(this.st.isEmpty()).toBeTrue()
+    })
+
+    it('should return false if the prop _n is greater than 0', () => {
+      spyOn(this.st, 'size').and.returnValue(1)
+
+      expect(this.st.isEmpty()).toBeFalse()
+    })
+  })
+
+  describe('SymbolTable#contains()', () => {
+    it('should return true if the get method returns a non-null value', () => {
+      spyOn(this.st, 'get').and.returnValue(0)
+
+      expect(this.st.contains('A')).toBeTrue()
+    })
+
+    it('should return false if the get method returns null as value', () => {
+      spyOn(this.st, 'get').and.returnValue(null)
+
+      expect(this.st.contains('A')).toBeFalse()
     })
   })
 
