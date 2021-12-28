4754eeaa0c9403ebb9a842efc429a85f859b34d4
Isaac Ramirez <iramirezc@live.com.mx>
2021-12-28 01:53:43 -0600

    Remove useless standard libraries: - StdIn - StdOut - StdRandom

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,8 +1,5 @@
 module.exports = {
   In: require('./in/in'),
   math: require('./math/math'),
-  Matrix: require('./matrix/matrix'),
-  StdIn: require('./std-in/std-in'),
-  StdOut: require('./std-out/std-out'),
-  StdRandom: require('./std-random/std-random')
+  Matrix: require('./matrix/matrix')
 }
